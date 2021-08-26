e2e8c019dc5fad84fc803931e01dcd68a68526bc
Marin <marin.petrunic@gmail.com>
2021-08-26 09:29:13 +0200

    initial react dapp

---
+++ src/connect.button.tsx
@@ -0,0 +1,24 @@
+import { Button, Box, Text } from "@chakra-ui/react";
+import { useEthers, useEtherBalance } from "@usedapp/core";
+import { formatEther } from "@ethersproject/units";
+
+export default function ConnectButton() {
+    const {activateBrowserWallet, account } = useEthers();
+    const etherBalance = useEtherBalance(account);
+  
+    function handleConnectWallet() {
+      activateBrowserWallet();
+    }
+  
+    return account ? (
+      <Box>
+        <Text className="balance" color="white" fontSize="md">
+            {etherBalance && parseFloat(formatEther(etherBalance)).toFixed(3)} ETH
+        </Text>
+      </Box>
+    ) : (
+      <Button className="connectBtn" onClick={handleConnectWallet}>
+        Connect to a wallet
+      </Button>
+    );
+  }
